491d7e36bb1cc5a09d120647bb6afd143b0e7a62
wvlasenko <wvlasenko@gmail.com>
2020-01-09 23:12:23 +0200

    Add java class car

--- src/ua/javabegin/javasintaxis/Car.java
+++ src/ua/javabegin/javasintaxis/Car.java
@@ -1,5 +1,27 @@
 package ua.javabegin.javasintaxis;
 
 public class Car {
+	private int i;
+	private String name;
+	
 
+	public Car(int i, String name) {
+		this.i = i;
+		this.name = name;
+	}
+	public void test () {
+		i=5;
+
+	}
+	public void go() {
+//		int count =1;
+//		count++;
+		System.out.println(i);
+	}
+	public int getI() {
+		return i;
+	}
+	public String getName() {
+		return name;
+	}
 }
