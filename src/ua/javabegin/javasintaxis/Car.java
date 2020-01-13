3d1475ea7d2c168b5b50e6c69d6752debee9f037
unknown <wvlasenko@gmail.com>
2020-01-13 12:12:15 +0200

    Add more variable to the class

--- src/ua/javabegin/javasintaxis/Car.java
+++ src/ua/javabegin/javasintaxis/Car.java
@@ -1,22 +1,29 @@
 package ua.javabegin.javasintaxis;
 
-public class Car {
-	private int i;
+public class Car { 
 	private String name;
 	
-
+	private int i=5,Z=6;
+	private int x=7;
+	private int j=7;
+	
 	public Car(int i, String name) {
 		this.i = i;
 		this.name = name;
 	}
 	public void test () {
 		i=5;
+		go (i);
 
 	}
-	public void go() {
-//		int count =1;
-//		count++;
+	public void go(int i) {
+// 		local variable
+		int count =0;
+		count++;
 		System.out.println(i);
+		if (count>8) {
+			int j=5;
+		}
 	}
 	public int getI() {
 		return i;
@@ -25,3 +32,4 @@ public class Car {
 		return name;
 	}
 }
+
