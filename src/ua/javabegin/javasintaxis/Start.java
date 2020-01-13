908962c6bd58899b3d15a45f5c94695f999c8aaa
unknown <wvlasenko@gmail.com>
2020-01-13 13:01:11 +0200

    Add class Start

---
+++ src/ua/javabegin/javasintaxis/Start.java
@@ -0,0 +1,17 @@
+package ua.javabegin.javasintaxis;
+
+public class Start {
+	public static void  main (String [] args) {
+		int i=4;
+		i--;
+		String s="Word";
+		
+//		System.out.println(i++);
+		System.out.println(i);
+//		System.out.println(++i);
+		System.out.println(s);
+		i*=5;
+		System.out.println(--i);
+	}
+
+}
